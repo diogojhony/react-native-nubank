050534121ee6d051187066d21657af597ce8041e
diogojhony <diogojhony@gmail.com>
2019-06-21 16:51:54 -0300

    feat(*): add menu

---
+++ src/components/Menu/index.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import QRCode from 'react-native-qrcode';
+
+import { Container, Code, Nav, NavItem, NavText , SignOutButton, SignOutButtonText} from './styles';
+
+export default function Menu() {
+    return (
+        <Container>
+            <Code>
+                <QRCode
+                    value="https://github.com/diogojhony"
+                    size={80}
+                    fgColor="#fff"
+                    bgColor="#13b15f"
+                    // fgColor="#8b10ab"
+                />
+            </Code>
+
+            <Nav>
+                <NavItem>
+                    <Icon name="help-outline" size={20} color="#fff" />
+                    <NavText>Me ajuda</NavText>
+                </NavItem>
+                <NavItem>
+                    <Icon name="person-outline" size={20} color="#fff" />
+                    <NavText>Perfil</NavText>
+                </NavItem>
+                <NavItem>
+                    <Icon name="credit-card" size={20} color="#fff" />
+                    <NavText>Configurar Cartão</NavText>
+                </NavItem>
+                <NavItem>
+                    <Icon name="smartphone" size={20} color="#fff" />
+                    <NavText>Configurações do app</NavText>
+                </NavItem>
+            </Nav>
+
+            <SignOutButton onPress={()=>{}}>
+                <SignOutButtonText>SAIR DO APP</SignOutButtonText>
+            </SignOutButton>
+        </Container>
+    );
+}
